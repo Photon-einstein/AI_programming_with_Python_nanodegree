e99c8c0b7b9e64e14f5372257ddfcf8b34c10031
Tiago Sousa <t.l.sousa@gmail.com>
2026-03-07 12:15:18 +0000

    Add more exercises

---
+++ 8-Neural_networks/4-Cross_entropy.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def cross_entropy(Y, P):
+    """
+    This function takes as input two lists Y and P, and returns the float
+    corresponding to their cross-entropy.
+    """
+    if len(Y) != len(P):
+        return 0
+    sum = 0.0
+    for i in range(len(Y)):
+        if Y[i] == 1:
+            sum -= np.log(P[i])
+        else:
+            sum -= np.log(1 - P[i])
+    return sum
